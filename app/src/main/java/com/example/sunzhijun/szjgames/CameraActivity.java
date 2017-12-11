bffc5cf76e459b02ce2b67fd15bc021877256b6a
sunzhijuns <2843359242@qq.com>
2017-12-12 02:35:27 +0800

    照片存储到本地

--- app/src/main/java/com/example/sunzhijun/szjgames/CameraActivity.java
+++ app/src/main/java/com/example/sunzhijun/szjgames/CameraActivity.java
@@ -1,28 +1,57 @@
 package com.example.sunzhijun.szjgames;
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class CameraActivity extends AppCompatActivity {
     private static int REQ_1=1;
+    private static int REQ_2=2;
     private ImageView mImageView;
+
+    private String mFilePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
         mImageView = (ImageView) findViewById(R.id.iv_camera);
+        mFilePath = Environment.getExternalStorageDirectory().getAbsolutePath();
+        mFilePath=mFilePath + File.separator + "temp.png";
     }
 
-    public void startCamera(View view){
+    public void startCamera1(View view){
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         startActivityForResult(intent,REQ_1);
 
+    }
+    public void startCamera2(View view){
+
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        ContentValues contentValues = new ContentValues(1);
+        contentValues.put(MediaStore.Images.Media.DATA, mFilePath);
+        Uri uri = this.getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,contentValues);
+
+        intent.putExtra(MediaStore.EXTRA_OUTPUT,uri);
+        startActivityForResult(intent,REQ_2);
+
+    }
+
+    public void customCamera(View view){
+        startActivity(new Intent(this,CustomCamera.class));
     }
 
     @Override
@@ -33,6 +62,23 @@ public class CameraActivity extends AppCompatActivity {
                 Bundle bundle = data.getExtras();
                 Bitmap bitmap = (Bitmap) bundle.get("data");
                 mImageView.setImageBitmap(bitmap);
+            }else if (requestCode == REQ_2){
+                FileInputStream fis = null;
+                try {
+                    fis = new FileInputStream(mFilePath);
+                    Bitmap bitmap = BitmapFactory.decodeStream(fis);
+                    mImageView.setImageBitmap(bitmap);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }finally {
+                    if (fis != null){
+                        try {
+                            fis.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
             }
         }
     }
---
+++ app/src/main/java/com/example/sunzhijun/szjgames/CustomCamera.java
@@ -0,0 +1,22 @@
+package com.example.sunzhijun.szjgames;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * Created by sunzhijun on 2017/12/12.
+ */
+
+public class CustomCamera extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.custom_camera);
+
+    }
+    public void capture(View view){
+        
+    }
+}
